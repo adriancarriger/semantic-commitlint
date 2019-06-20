e6014fa4faa1158c3da211244b4286bef08b31ce
Adrian Carriger <hello@adriancarriger.com>
2019-06-19 19:04:12 -0500

    chore(deps): update read-pkg-up

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,8 @@
 import * as readPkgUp from 'read-pkg-up';
 
 export async function getSemanticCommitlintConfig() {
-  const { pkg } = await readPkgUp();
-  return pkg && pkg.semanticCommitlint ? pkg.semanticCommitlint : {};
+  const { package: closestPackage } = await readPkgUp();
+  return closestPackage && closestPackage.semanticCommitlint
+    ? closestPackage.semanticCommitlint
+    : {};
 }
